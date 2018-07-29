f194bc85221a9b17d6c2fd73e45826971faee999
Chris Breneman <crispy@cluenet.org>
2018-07-28 23:22:14 -0400

    More build script

--- build-stls.sh
+++ build-stls.sh
@@ -12,13 +12,20 @@ export HEIGHT=$2
 export NUMRINGS=$3
 
 function buildPart {
+	echo "${1} ..."
 	SCADFILE="${1}.scad"
 	STLFILE="cryptex-${NUMRINGS}ring-${DIAMETER}x${HEIGHT}-${1}.stl"
 	PNGFILE="cryptex-${NUMRINGS}ring-${DIAMETER}x${HEIGHT}-${1}.png"
-#	$OPENSCAD -o "$STLFILE" -D "compartmentDiameter=$DIAMETER" -D "compartmentHeight=$HEIGHT" -D "numRings=$NUMRINGS" "$SCADFILE"
+	echo "    Building $SCADFILE -> $STLFILE"
+	$OPENSCAD -o "$STLFILE" -D "compartmentDiameter=$DIAMETER" -D "compartmentHeight=$HEIGHT" -D "numRings=$NUMRINGS" "$SCADFILE"
+	echo "    Building $SCADFILE -> $PNGFILE"
 	$OPENSCAD -o "$PNGFILE" -D "compartmentDiameter=$DIAMETER" -D "compartmentHeight=$HEIGHT" -D "numRings=$NUMRINGS" --render --imgsize=600,600 "$SCADFILE"
 }
 
+buildPart innershell
+buildPart outershell
 buildPart ring-and-labelring
-
+buildPart lockring
+buildPart endcap
+buildPart ringseparatortool
 
